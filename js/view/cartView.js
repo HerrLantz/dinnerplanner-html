aabc777b42ac8b46bfd883f2ac220842fc351618
Patric Lantz <patricla@kth.se>
2019-01-20 21:18:27 +0100

    Add desktop layout for cartView

--- js/view/cartView.js
+++ js/view/cartView.js
@@ -45,74 +45,76 @@ var CartView = function(container, model) {
 
   container.html(`
     <div id="dinnerOverview">
-      <div id="cartViewHeader">
-        <h1>My Dinner</h1>
-        <div id="burgerMenu">
-          <svg id="burgerIcon" height="25" width="30">
-            <line x1="0" y1="5" x2="30" y2="5" />
-            <line x1="0" y1="12" x2="30" y2="12" />
-            <line x1="0" y1="19" x2="30" y2="19" />
-          </svg>
-        </div>
-      </div>
-      <div id="peopleSelector">
-        <label>
-          People
-          <input
-            type="number"
-            value="${model.getNumberOfGuests()}"
-            max="999"
-            min="0"
-          />
-        </label>
-      </div>
-      <div id="confirmationBox">
-        <div class="divider">
-          <span>Dish name</span>
-          <span>Cost</span>
-        </div>
+      <div id="dinnerPlan">
+          <div id="cartViewHeader">
+              <h1>My Dinner</h1>
+              <div id="burgerMenu">
+              <svg id="burgerIcon" height="25" width="30">
+                  <line x1="0" y1="5" x2="30" y2="5" />
+                  <line x1="0" y1="12" x2="30" y2="12" />
+                  <line x1="0" y1="19" x2="30" y2="19" />
+              </svg>
+              </div>
+          </div>
+          <div id="peopleSelector">
+              <label>
+              People
+              <input
+                  type="number"
+                  value="${model.getNumberOfGuests()}"
+                  max="999"
+                  min="0"
+              />
+              </label>
+          </div>
+          <div id="confirmationBox">
+              <div class="divider">
+              <span>Dish name</span>
+              <span>Cost</span>
+              </div>
 
-        <table id="dinnerTable">
-          <tr>
-            <td>Korvstroganoff</td>
-            <td>7000kr</td>
-          </tr>
-          <tr>
-            <td>Korvstroganoff</td>
-            <td>7000kr</td>
-          </tr>
-          <tr>
-            <td>Korvstroganoff</td>
-            <td>7000kr</td>
-          </tr>
-          <tr>
-            <td>Korvstroganoff</td>
-            <td>7000kr</td>
-          </tr>
-        </table>
-        <p id="totalPrice">SEK 100000</p>
-        <div id="confirmDinner">
-          <button class="primaryButton" disabled>
-          Confirm Dinner
-          </button>
-        </div>
+              <table id="dinnerTable">
+              <tr>
+                  <td>Korvstroganoff</td>
+                  <td>7000kr</td>
+              </tr>
+              <tr>
+                  <td>Korvstroganoff</td>
+                  <td>7000kr</td>
+              </tr>
+              <tr>
+                  <td>Korvstroganoff</td>
+                  <td>7000kr</td>
+              </tr>
+              <tr>
+                  <td>Korvstroganoff</td>
+                  <td>7000kr</td>
+              </tr>
+              </table>
+              <p id="totalPrice">SEK 100000</p>
+              <div id="confirmDinner">
+              <button class="primaryButton" disabled>
+              Confirm Dinner
+              </button>
+              </div>
+          </div>
       </div>
-    </div>
-
-    <div id="dinnerFinder">
-      <div id="searchPanel">
-        <h1>FIND A DISH</h1>
-        <input type="text" placeholder="Enter keywords"/>
-        <select id="dishes">
-          ${dishTypesToString()}
-        </select>
-        <button class="primaryButton">
-          search
-        </button>
-      </div>
-      
-      <div id="resultPanel">
-        ${showResults('', '')}
+      <div id="dinnerFinder">
+          <div id="searchPanel">
+              <h1>FIND A DISH</h1>
+              <div id="filterOptions">
+                <input type="text" placeholder="Enter keywords"/>
+                <select id="dishes">
+                    ${dishTypesToString()}
+                </select>
+                <button class="primaryButton">
+                search
+                </button>
+              </div>
+          </div>
+          <div id="resultPanel">
+              ${showResults('', '')}
+          </div>
       </div>
     </div>
   `);
