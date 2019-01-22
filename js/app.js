d16644fbab6a182213627435edec48f9dc16b19d
shappe <shappan@gmail.com>
2019-01-22 18:35:35 +0100

    Remove(d)

--- js/app.js
+++ js/app.js
@@ -7,6 +7,7 @@ $(function() {
   // And create the instance of ExampleView
   // var exampleView = new ExampleView($('#exampleView'), model)
   var cartView = new CartView($('#cartView'), model);
+  var dishDetailsView = new DishDetailsView($('#dishDetailsView'), model);
 
   /**
    * IMPORTANT: app.js is the only place where you are allowed to
