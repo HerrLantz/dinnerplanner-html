02d12eb05546b3f101c26cf19b31c3e628a2c095
Patric Lantz <patricla@kth.se>
2019-02-03 22:13:52 +0100

    Added hide function that hides the view

--- js/view/headerView.js
+++ js/view/headerView.js
@@ -1,9 +1,16 @@
 class HeaderView {
   constructor(container, model) {
     this.container = container;
+
+    this.displayProperty = container.style.display;
+  }
+
+  hide() {
+    this.container.style.display = 'none';
   }
 
   render() {
+    this.container.style.display = this.displayProperty;
     this.container.innerHTML = `<h1>Dinner Planner</h1>`;
   }
 }
