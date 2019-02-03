bfbc662d66a25b304eb3aa6cbfbfacdbd7221bff
Patric Lantz <patricla@kth.se>
2019-02-03 22:14:37 +0100

    Refactor, finderView is now a class not a function

--- js/view/finderView.js
+++ js/view/finderView.js
@@ -1,37 +1,47 @@
-var FinderView = function(container, model) {
-  var dishTypesToString = function() {
+class FinderView {
+  constructor(container, model) {
+    this.container = container;
+
+    this.model = model;
+    this.displayProperty = container.style.display;
+  }
+
+  /**
+   * Hides the view
+   */
+  hide() {
+    this.container.style.display = 'none';
+  }
+
+  /**
+   * Populates a select element with all the types of dishes
+   */
+  dishTypesToString() {
     var types = `<option value="All" selected>All</option>\n`;
-    for (const dishType in model.getAllTypes()) {
+    for (const dishType in this.model.getAllTypes()) {
       types += `<option value="${dishType}">${dishType}</option>\n`;
     }
     return types;
-  };
+  }
 
-  const populateResultPanel = (type, filter) => {
-    // Get all types of dishes
-    var dishes = model.getAllDishes('', '');
-
-    dishes.forEach(dish => {
-      new DishView($('#resultPanel'), model, dish.id, false);
-    });
-  };
-
-  $(document).ready(function() {
-    populateResultPanel('', '');
-  });
-  container.html(`
+  /**
+   * Renders the view
+   */
+  render() {
+    this.container.style.display = this.displayProperty;
+    this.container.innerHTML = `
     <div id="dinnerFinder">
       <div id="searchPanel">
         <h1>FIND A DISH</h1>
         <div id="filterOptions">
           <input type="text" placeholder="Enter keywords" />
-          <select id="dishes">${dishTypesToString()}</select>
+          <select id="dishes">${this.dishTypesToString()}</select>
           <button class="primaryButton">search</button>
         </div>
       </div>
       <div id="resultPanelWrapper">
         <div id="resultPanel" />
       </div>
-    </div>
-  `);
-};
+    </div>`;
+  }
+}
