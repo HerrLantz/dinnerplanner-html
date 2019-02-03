eb6ec1ddc35a156fed024ba1e51e05fb27e40cf8
Patric Lantz <patricla@kth.se>
2019-02-03 18:44:56 +0100

    Refactor homeView from function to class

--- js/view/homeView.js
+++ js/view/homeView.js
@@ -1,11 +1,34 @@
-var HomeView = function(container, model) {
-  const loremIpsum =
-    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean at metus dui. Morbi consectetur lorem risus. Pellentesque vel ante sit amet nisl dictum dictum eu vel odio. Morbi sit amet massa nunc. Praesent sagittis tortor non diam rhoncus blandit et eget sapien. Nulla ligula enim, lobortis quis enim et, aliquet pulvinar dolor. Aliquam sem lacus, porttitor vel nibh vel, consequat congue orci. Pellentesque quis lorem et orci efficitur egestas. Quisque cursus ligula felis, nec congue ante placerat at.';
+class HomeView {
+  constructor(container, model) {
+    this.loremIpsum = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean at metus dui. Morbi consectetur lorem risus. 
+    Pellentesque vel ante sit amet nisl dictum dictum eu vel odio. Morbi sit amet massa nunc. Praesent sagittis tortor non diam rhoncus
+     blandit et eget sapien. Nulla ligula enim, lobortis quis enim et, aliquet pulvinar dolor. Aliquam sem lacus, porttitor vel nibh vel,
+      consequat congue orci. Pellentesque quis lorem et orci efficitur egestas. Quisque cursus ligula felis, nec congue ante placerat at.`;
 
-  container.html(
-    `<p>${loremIpsum}</p>
-    <button class="primaryButton">
-        Create new dinner
-    </button>`
-  );
-};
+    this.container = container;
+  }
+
+  welcomeText() {
+    return `<p>${this.loremIpsum}</p>`;
+  }
+
+  createDinnerButton() {
+    return `<button class="primaryButton">Create new dinner</button>`;
+  }
+
+  /**
+   * Updates the view with new data
+   */
+  update() {
+    render();
+  }
+
+  /**
+   * Render the homeView with a welcome text and a button that takes the user to
+   * the page where he/she can create a dinner plan
+   */
+  render() {
+    this.container.innerHTML = `${this.welcomeText() +
+      this.createDinnerButton()}`;
+  }
+}
