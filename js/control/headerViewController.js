0005d38ad62f42bbb8ea27be1a256ea1c90819a6
Patric Lantz <patricla@kth.se>
2019-02-04 15:24:28 +0100

    Dont remember what i did

--- js/control/headerViewController.js
+++ js/control/headerViewController.js
@@ -1,5 +1,7 @@
 class HeaderViewController {
   constructor(view, model, newView) {
+    console.log(view);
+
     view.getElementsByTagName('h1')[0].addEventListener('click', () => {
       newView();
     });
