160ae5d28e7e65c255b1c6e02aa64cfb2efd879d
Patric Lantz <patricla@kth.se>
2019-02-06 21:13:45 +0100

    Removed log

--- js/control/headerViewController.js
+++ js/control/headerViewController.js
@@ -1,7 +1,5 @@
 class HeaderViewController {
   constructor(view, model, newView, hideView) {
-    console.log(view);
-
     view.getElementsByTagName('h1')[0].addEventListener('click', () => {
       hideView();
       newView();
