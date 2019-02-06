6ae93460dfa2b49bc639dd7c4bdcdfcaf8d4e723
Patric Lantz <patricla@kth.se>
2019-02-06 17:56:04 +0100

    Indentation fix

--- js/observable.js
+++ js/observable.js
@@ -1,17 +1,19 @@
 class Observable {
-    constructor(){
-	   this._observers = [];
-   }
+  constructor() {
+    this._observers = [];
+  }
 
-    addObserver(observer){
-	   this._observers.push(observer);
+  addObserver(observer) {
+    this._observers.push(observer);
+  }
+
+  notifyObservers(changeDetails) {
+    for (var i = 0; i < this._observers.length; i++) {
+      this._observers[i].update(this, changeDetails);
     }
-   
-    notifyObservers(changeDetails) {
-           for(var i=0; i<this._observers.length; i++) {
-                 this._observers[i].update(this, changeDetails);
-           }	
-     }
+  }
 
-     removeObserver(observer){  /* remove observer from array */}
+  removeObserver(observer) {
+    /* remove observer from array */
+  }
 }
