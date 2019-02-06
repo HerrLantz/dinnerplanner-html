1fc54134841fe2d8190db79af9748b6e45929994
Patric Lantz <patricla@kth.se>
2019-02-06 17:35:15 +0100

    Fixed resize problem of window

--- js/control/cartViewController.js
+++ js/control/cartViewController.js
@@ -1,25 +1,41 @@
 class CartViewController {
   constructor(view, model, searchPanel) {
     this.visible = true;
+    this.view = view;
+    this.model = model;
+    this.searchPanel = searchPanel;
+
+    window.addEventListener('resize', () => {
+      if (window.innerWidth >= 800) {
+        this.expandMenu();
+      }
+    });
 
     /**
      * Event listener for burger menu to expand/collapse the menu
      */
     view.querySelector('#burgerMenu').addEventListener('click', () => {
       if (this.visible) {
-        // TODO: Add hide(), cartViewHeader span in views
-        view.querySelector('#confirmationBox').style.display = 'none';
-        view.querySelector('#peopleSelector').style.display = 'none';
-        view.querySelector('#cartViewHeader span').style.display = 'none';
-        searchPanel.style.display = 'none';
+        this.collapseMenu();
         this.visible = false;
       } else {
-        view.querySelector('#confirmationBox').style.display = 'flex';
-        view.querySelector('#peopleSelector').style.display = 'flex';
-        view.querySelector('#cartViewHeader span').style.display = 'flex';
-        searchPanel.style.display = 'flex';
+        this.expandMenu();
         this.visible = true;
       }
     });
+  }
+
+  collapseMenu() {
+    this.view.querySelector('#confirmationBox').style.display = 'none';
+    this.view.querySelector('#peopleSelector').style.display = 'none';
+    this.view.querySelector('#cartViewHeader span').style.display = 'flex';
+    this.searchPanel.style.display = 'none';
+  }
+
+  expandMenu() {
+    this.view.querySelector('#confirmationBox').style.display = 'flex';
+    this.view.querySelector('#peopleSelector').style.display = 'flex';
+    this.view.querySelector('#cartViewHeader span').style.display = 'none';
+    this.searchPanel.style.display = 'flex';
   }
 }
