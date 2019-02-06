2afc6906722bff5b99ab96370419d109972bce23
Patric Lantz <patricla@kth.se>
2019-02-06 21:18:57 +0100

    Removed old code

--- js/view/overView.js
+++ js/view/overView.js
@@ -2,11 +2,11 @@ var OverView = function(container, model) {
   $(document).ready(function() {
     var selectedDishes = model.getSelectedDishes();
 
-    for (let type in selectedDishes) {
-      new DishView($('#resultPanel'), model, selectedDishes[type], true);
-    }
+    // for (let type in selectedDishes) {
+    //   new DishView($('#resultPanel'), model, selectedDishes[type], true);
+    // }
 
-    $('.total').html('Total:<br/>' + model.getTotalMenuPrice() + ' SEK');
+    // $('.total').html('Total:<br/>' + model.getTotalMenuPrice() + ' SEK');
   });
 
   container.html(`
