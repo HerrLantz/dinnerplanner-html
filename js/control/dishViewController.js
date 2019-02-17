c4bdd032d44df68eac0ab1ee262de075c1626cfb
Shapour Jahanshahi <shapour@kth.se>
2019-02-17 01:00:02 +0100

    Removed old comments

--- js/control/dishViewController.js
+++ js/control/dishViewController.js
@@ -12,10 +12,6 @@ class DishViewController {
     if (changeDetails.type === 'search_update') {
       for (let dish of this.view) {
         console.log(dish.getAttribute('dishid'));
-        // dish.addEventListener('click', () => {
-        //   let dishID = dish.getAttribute('dishid');
-        //   model.addDishToMenu(dishID);
-        // });
         dish.addEventListener('click', () => {
           let dishID = dish.getAttribute('dishid');
           model.setDishDetailsID(dishID);
