3129b962712e677e0f7e39e3709aca9b17a1feb6
Shapour Jahanshahi <shapour@kth.se>
2019-02-17 01:39:38 +0100

    Removed unused code.

--- js/view/dishDetailsView.js
+++ js/view/dishDetailsView.js
@@ -1,25 +1,3 @@
-// var DishDetailsView = function(container, model, dish) {
-//   $(document).ready(function() {
-//     new IngredientsView($('.ingredients'), model, dish);
-//   });
-//   container.html(`
-//       <div id="dishDesc">
-//         <div id="dishDetails">
-//           <h1>${dish.name}</h1>
-//           <div class="dishImageWrapper">
-//             <img src="${model.imgPath + dish.image}" class="dishImage"/>
-//           </div>
-//           <p>${dish.description}</p>
-//           <button class="backButton">back to search</button>
-//         </div>
-//         <div class="ingredients">
-//         </div>
-//       </div>
-//       <h1>PREPARATION</h1>
-//       <p>${dish.preparation}</p>
-//     `);
-// };
-
 class DishDetailsView {
   constructor(container, model, controller) {
     model.addObserver(this);
