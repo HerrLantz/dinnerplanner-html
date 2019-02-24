9c96ac203d6310b9aad73477288196ceb6b98ab7
Shapour Jahanshahi <shapour@kth.se>
2019-02-24 14:35:30 +0100

    Removed setTimeout

--- js/control/dishViewController.js
+++ js/control/dishViewController.js
@@ -11,19 +11,14 @@ class DishViewController {
   update(model, changeDetails) {
     
     if (changeDetails.type === 'search_update' || changeDetails.type === 'cart_update') {
-      setTimeout(
-      () => {
       for (let dish of this.view) {
-        dish.addEventListener('click', () => {
-          console.log("MUUUUUUUU");
-          
+        dish.addEventListener('click', () => {          
           let dishID = dish.getAttribute('dishid');
           model.setDishDetailsID(dishID);
           this.hideView();
           this.newView();
         });
       }
-    }, 3000);
     }
   }
 }
